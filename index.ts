f679e6d9377a349a163d677ca5605d6d7a4b27d0
Moli <molireyero@pm.me>
2022-10-08 21:06:42 +0200

    Serving static assets (https://deno.com/deploy/docs/serve-static-assets)

--- index.ts
+++ index.ts
@@ -1,11 +1,45 @@
-import { Application } from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-const app = new Application();
+async function handleRequest(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url);
 
-app
-  .get("/", (c) => {
-    return "Hello World";
-  })
-  .static("/", "./src")
-  .start( { port: 80 });
-  //.start({ port: 8000 });
+  // This is how the server works:
+  // 1. A request comes in for a specific asset.
+  // 2. We read the asset from the file system.
+  // 3. We send the asset back to the client.
+
+  // Check if the request is for style.css.
+  if (pathname.startsWith("/style.css")) {
+    // Read the style.css file from the file system.
+    const fileCss = await Deno.readFile("./src/style.css");
+    // Respond to the request with the style.css file.
+    return new Response(fileCss, {
+      headers: {
+        "content-type": "text/css",
+      },
+    });
+  }
+
+  // Check if the request is for style.css.
+  if (pathname.startsWith("/script.js")) {
+    // Read the style.css file from the file system.
+    const fileJs = await Deno.readFile("./src/script.js");
+    // Respond to the request with the style.css file.
+    return new Response(fileJs, {
+      headers: {
+        "content-type": "script/js",
+      },
+    });
+  }
+
+  // Read the style.css file from the file system.
+  const fileHtml = await Deno.readFile("./src/index.html");
+  // Respond to the request with the style.css file.
+  return new Response(fileHtml, {
+    headers: {
+      "content-type": "text/html; charset=utf-8",
+    },
+  });
+}
+
+serve(handleRequest);
