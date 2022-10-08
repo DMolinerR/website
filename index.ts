0b068df31596f8ab6018d6819654de87ebc11949
Moli <molireyero@pm.me>
2022-10-08 20:50:40 +0200

    Port changed from 8000 to 80

--- index.ts
+++ index.ts
@@ -3,5 +3,9 @@ import { Application } from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
 const app = new Application();
 
 app
+  .get("/", (c) => {
+    return "Hello World";
+  })
   .static("/", "./src")
-  .start({ port: 8000 });
+  .start( { port: 80 });
+  //.start({ port: 8000 });
